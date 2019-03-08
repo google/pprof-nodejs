8de4c0a81ce9e7437dc2361f679c6594024d2927
Maggie Nolan <nolanmar@google.com>
2019-03-08 07:58:32 -0800

    chore: add method to encode profile

---
+++ ts/test/test-profile-encoder.ts
@@ -0,0 +1,47 @@
+/**
+ * Copyright 2019 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as pify from 'pify';
+import {gunzip as gunzipPromise, gunzipSync} from 'zlib';
+
+import {perftools} from '../../proto/profile';
+import {encode, encodeSync} from '../src/profile-encoder';
+
+import {decodedTimeProfile, timeProfile} from './profiles-for-tests';
+
+const assert = require('assert');
+const gunzip = pify(gunzipPromise);
+
+describe('profile-encoded', () => {
+  describe('encode', () => {
+    it('should encode profile such that the encoded profile can be decoded',
+       async () => {
+         const encoded = await encode(timeProfile);
+         const unzipped = await gunzip(encoded);
+         const decoded = perftools.profiles.Profile.decode(unzipped);
+         assert.deepEqual(decoded, decodedTimeProfile);
+       });
+  });
+  describe('encodeSync', () => {
+    it('should encode profile such that the encoded profile can be decoded',
+       () => {
+         const encoded = encodeSync(timeProfile);
+         const unzipped = gunzipSync(encoded);
+         const decoded = perftools.profiles.Profile.decode(unzipped);
+         assert.deepEqual(decoded, decodedTimeProfile);
+       });
+  });
+});
