ff3ae165d3a0309ba310ca08cfbe229e0e47edaf
Maggie Nolan <nolanmar@google.com>
2019-03-26 08:19:05 -0700

    address comments

--- system-test/system_test.sh
+++ system-test/system_test.sh
@@ -13,20 +13,20 @@ retry() {
 cd $(dirname $0)
 
 if [[ -z "$BINARY_HOST" ]]; then
-  BUILD_BINARIES=true
+  ADDITIONAL_PACKAGES="python g++ make"
 fi
 
 for i in 6 8 10 11; do
   # Test Linux support for the given node version.
   retry docker build -f Dockerfile.linux --build-arg NODE_VERSION=$i \
-      --build-arg BUILD_BINARIES="$BUILD_BINARIES" -t node$i-linux .
+      --build-arg ADDITIONAL_PACKAGES="$ADDITIONAL_PACKAGES" -t node$i-linux .
 
   docker run  -v $PWD/..:/src -e BINARY_HOST="$BINARY_HOST" node$i-linux \
       /src/system-test/test.sh
 
   # Test Alpine support for the given node version.
   retry docker build -f Dockerfile.node$i-alpine \
-      --build-arg BUILD_BINARIES="$BUILD_BINARIES" -t node$i-alpine .
+      --build-arg ADDITIONAL_PACKAGES="$ADDITIONAL_PACKAGES" -t node$i-alpine .
 
   docker run -v $PWD/..:/src -e BINARY_HOST="$BINARY_HOST" node$i-alpine \
       /src/system-test/test.sh
