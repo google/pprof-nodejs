ea91a5f73adb0ab98a9dc99aa044e7ec4c914df6
Margaret Nolan <nolanmar@google.com>
2019-04-04 13:20:14 -0700

    chore: enable running tests with v8-canary (#40)

--- system-test/system_test.sh
+++ system-test/system_test.sh
@@ -16,13 +16,27 @@ if [[ -z "$BINARY_HOST" ]]; then
   ADDITIONAL_PACKAGES="python g++ make"
 fi
 
-for i in 6 8 10 11; do
+if [[ "$RUN_ONLY_V8_CANARY" == "true" ]]; then
+  NVM_NODEJS_ORG_MIRROR="https://nodejs.org/download/v8-canary"
+  NODE_VERSIONS=(node)
+else
+  NODE_VERSIONS=(6 8 10 11)
+fi
+
+for i in ${NODE_VERSIONS[@]}; do
   # Test Linux support for the given node version.
   retry docker build -f Dockerfile.linux --build-arg NODE_VERSION=$i \
-      --build-arg ADDITIONAL_PACKAGES="$ADDITIONAL_PACKAGES" -t node$i-linux .
+      --build-arg ADDITIONAL_PACKAGES="$ADDITIONAL_PACKAGES" \
+      --build-arg  NVM_NODEJS_ORG_MIRROR="$NVM_NODEJS_ORG_MIRROR" \
+      -t node$i-linux .
 
   docker run  -v $PWD/..:/src -e BINARY_HOST="$BINARY_HOST" node$i-linux \
       /src/system-test/test.sh
+
+  # Skip running on alpine if NVM_NODEJS_ORG_MIRROR is specified.
+  if [[ -z "$NVM_NODEJS_ORG_MIRROR" ]]; then
+    continue
+  fi
 
   # Test Alpine support for the given node version.
   retry docker build -f Dockerfile.node$i-alpine \
