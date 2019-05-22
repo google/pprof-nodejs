3ae20f20d384721c811bee63c251af06e80b6774
Margaret Nolan <nolanmar@google.com>
2019-05-22 16:11:45 -0700

    fix: retry npm install in system test when command hangs (#65)
    * chore: retry npm install in system test when command hangs
    
    * make npm_install work on alpine

--- system-test/test.sh
+++ system-test/test.sh
@@ -6,6 +6,18 @@ retry() {
   "${@}" || "${@}" || "${@}" || return 1
 }
 
+function timeout_after() {
+  if [ -f /bin/busybox ]; then
+    timeout -t "${@}"
+  else
+    timeout "${@}"
+  fi
+}
+
+npm_install() {
+  timeout_after 60 npm install "${@}"
+}
+
 set -eox pipefail
 cd $(dirname $0)/..
 
@@ -16,9 +28,9 @@ NODEDIR=$(dirname $(dirname $(which node)))
 # contains unreleased fixes that allow the native component to be compiled
 # with Node's V8 canary build.
 [ -z $NVM_NODEJS_ORG_MIRROR ] \
-    || retry npm install https://github.com/nodejs/nan.git
+    || retry npm_install https://github.com/nodejs/nan.git
 
-retry npm install --nodedir="$NODEDIR" \
+retry npm_install --nodedir="$NODEDIR" \
     ${BINARY_HOST:+--pprof_binary_host_mirror=$BINARY_HOST} >/dev/null
 
 npm run compile
@@ -30,8 +42,8 @@ TESTDIR=$(mktemp -d)
 cp -r "$PWD/system-test/busybench" "$TESTDIR"
 cd "$TESTDIR/busybench"
 
-retry npm install pify @types/pify typescript gts @types/node >/dev/null
-retry npm install --nodedir="$NODEDIR" \
+retry npm_install pify @types/pify typescript gts @types/node >/dev/null
+retry npm_install --nodedir="$NODEDIR" \
     ${BINARY_HOST:+--pprof_binary_host_mirror=$BINARY_HOST} \
     "$PROFILER">/dev/null
 
