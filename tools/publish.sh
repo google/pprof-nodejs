1840de05610d26e55e1b66edc7ceb73da2662575
Maggie Nolan <nolanmar@google.com>
2020-05-19 19:02:46 -0700

    chore: add backoff to retry function used in e2e test (#120)
    * chore: add backoff to retry function used in e2e test
    
    * address comments

--- tools/publish.sh
+++ tools/publish.sh
@@ -13,12 +13,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-retry() {
-  for i in {1..3}; do
-    "${@}" && return 0
-  done
-  return 1
-}
+
+. $(dirname $0)/retry.sh
 
 set -eo pipefail
 
