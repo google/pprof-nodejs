d1455a7f32b966fd9a0b76a95704310e9135aa0a
Garrett Wang <garrettwang@google.com>
2021-10-12 10:22:01 -0700

    Modify how we fetch Python 3.6

--- system-test/test.sh
+++ system-test/test.sh
@@ -22,7 +22,10 @@ set -eox pipefail
 cd $(dirname $0)/..
 
 # Install supported Python version to build Node.js binaries with node-gyp.
-sudo apt-get update && sudo apt-get install python3.6
+sudo apt-get install software-properties-common
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt-get update
+sudo apt-get install python3.6
 
 NODEDIR=$(dirname $(dirname $(which node)))
 
