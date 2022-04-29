239c52ae1fa8790bfdf3d70bab9733e252778609
Amarin Phaosawasdi <amarinph@google.com>
2022-04-28 17:17:42 -0700

    [DO NOT MERGE] Update node versions to use in integration test

--- system-test/system_test.sh
+++ system-test/system_test.sh
@@ -12,9 +12,9 @@ cd $(dirname $0)
 
 if [[ -z "$BINARY_HOST" ]]; then
   ADDITIONAL_PACKAGES="python3 g++ make"
-  NODE_VERSIONS=(10 12 14 15 16 node)
+  NODE_VERSIONS=(12 14 16 17 node)
 else
-  NODE_VERSIONS=(10 12 14 15 16)
+  NODE_VERSIONS=(12 14 16 17)
 fi
 
 for i in ${NODE_VERSIONS[@]}; do
@@ -26,13 +26,9 @@ for i in ${NODE_VERSIONS[@]}; do
   docker run  -v $PWD/..:/src -e BINARY_HOST="$BINARY_HOST" node$i-linux \
       /src/system-test/test.sh
 
-  # Test support for accurate line numbers with node versions supporting this
-  # feature.
-  if [ "$i" != "10" ]; then
-    docker run  -v $PWD/..:/src -e BINARY_HOST="$BINARY_HOST" \
-        -e VERIFY_TIME_LINE_NUMBERS="true" node$i-linux \
-        /src/system-test/test.sh
-  fi
+  docker run  -v $PWD/..:/src -e BINARY_HOST="$BINARY_HOST" \
+      -e VERIFY_TIME_LINE_NUMBERS="true" node$i-linux \
+      /src/system-test/test.sh
 
   # Test Alpine support for the given node version.
   retry docker build -f Dockerfile.node$i-alpine \
