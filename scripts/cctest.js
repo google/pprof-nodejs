0af658cda1f2794f196dc77403d3247ce675ef5b
Stephen Belanger <admin@stephenbelanger.com>
2022-06-14 12:13:02 -0700

    Make prebuilds with prebuildify action (#33)

--- scripts/cctest.js
+++ scripts/cctest.js
@@ -1,9 +1,23 @@
 'use strict'
 
+const { execSync } = require('child_process')
 const { existsSync } = require('fs')
 const { join } = require('path')
+const rimraf = require('rimraf')
 
 const name = process.argv[2] || 'test_dd_pprof'
+
+const cmd = [
+  'node-gyp rebuild',
+  '--release',
+  '--jobs=max',
+  '--build_v8_with_gn=false',
+  '--v8_enable_pointer_compression=""',
+  '--v8_enable_31bit_smis_on_64bit_arch=""',
+  '--enable_lto=false'
+].join(' ')
+
+execSync(cmd, { stdio: [0, 1, 2] })
 
 function findBuild (mode) {
   const path = join(__dirname, '..', 'build', mode, name) + '.node'
@@ -21,5 +35,7 @@ if (!path) {
   console.error(`No ${name} build found`)
   process.exitCode = 1
 } else {
-  require(path)
+  execSync(`node ${path}`, { stdio: [0, 1, 2] })
 }
+
+rimraf.sync('./build')
